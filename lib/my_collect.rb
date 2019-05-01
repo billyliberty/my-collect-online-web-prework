1c04056163da4958b64692c479ad37201649f6da
Joshua Crum <joshua.crum@gmail.com>
2019-05-01 16:54:00 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,9 @@
-def my_collect(empty_array)
+def my_collect(collection)
   i = 0
-  new_array = []
-  empty_array.collect do |array|
-    new_array << yield(arr[i])
+  new_collection = []
+  while i < collection.length
+    new_collection << yield(collection[i])
     i += 1
   end
-  return new_array
+  return new_collection
 end
